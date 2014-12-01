10483e40ead6cf23754606d903d11be751afb055
Shrikant <shrikant.havale@yahoo.com>
2014-12-01 12:18:15 +0100

    not used style deleted

--- SalesManagement/src/com/shrikanthavale/salesmanagement/StartPageActivity.java
+++ SalesManagement/src/com/shrikanthavale/salesmanagement/StartPageActivity.java
@@ -95,7 +95,7 @@ public class StartPageActivity extends Activity {
 	public void onBackPressed() {
 		Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         finish();
         System.exit(0);
