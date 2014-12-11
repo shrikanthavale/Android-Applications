45d203cd45d26fcdd72dd3e4415d6b26568c16cc
Shrikant <shrikant.havale@yahoo.com>
2014-12-11 01:15:15 +0100

    Signed-off-by: Shrikant <shrikant.havale@yahoo.com>

--- SalesManagementMonkeyTest/src/com/shrikanthavale/salesmanagement/test/RandomTest_A.java
+++ SalesManagementMonkeyTest/src/com/shrikanthavale/salesmanagement/test/RandomTest_A.java
@@ -1,6 +1,7 @@
 package com.shrikanthavale.salesmanagement.test;
 
 
+import com.robotium.solo.Solo;
 import com.shrikanthavale.salesmanagement.StartPageActivity;
 
 import eu.fbk.se.androidmonkey.Monkey;
@@ -11,17 +12,162 @@ import android.view.Display;
 
 public class RandomTest_A extends ActivityInstrumentationTestCase2<StartPageActivity> {
 	
-	private static final int NUM_EVENTS = 1000;
+	private static final int NUM_EVENTS = 500;
 	private static final String packageToTest = "com.shrikanthavale.salesmanagement";
+	private Solo robotiumSolo;
 	
 	public RandomTest_A(){
-		super(packageToTest, StartPageActivity.class);
+		super(StartPageActivity.class);
 	}
 
 	/**
 	 * Trigger the monkey tester
 	 */
-	public void testMonkeyEvents(){
+	public void testMonkeyEvents1(){
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		Instrumentation inst = getInstrumentation();
+		PackageManager pm = getActivity().getPackageManager();
+
+		Monkey monkey = new Monkey(display, packageToTest, inst, pm);
+		
+		for (int i = 0; i < NUM_EVENTS; i++){
+			monkey.nextRandomEvent();
+		}
+		
+	}
+	
+	/**
+	 * Trigger the monkey tester
+	 */
+	public void testMonkeyEvents2(){
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		Instrumentation inst = getInstrumentation();
+		PackageManager pm = getActivity().getPackageManager();
+
+		Monkey monkey = new Monkey(display, packageToTest, inst, pm);
+		
+		for (int i = 0; i < NUM_EVENTS; i++){
+			monkey.nextRandomEvent();
+		}
+		
+	}
+	
+	/**
+	 * Trigger the monkey tester
+	 */
+	public void testMonkeyEvents3(){
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		Instrumentation inst = getInstrumentation();
+		PackageManager pm = getActivity().getPackageManager();
+
+		Monkey monkey = new Monkey(display, packageToTest, inst, pm);
+		
+		for (int i = 0; i < NUM_EVENTS; i++){
+			monkey.nextRandomEvent();
+		}
+		
+	}
+	
+	/**
+	 * Trigger the monkey tester
+	 */
+	public void testMonkeyEvents4(){
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		Instrumentation inst = getInstrumentation();
+		PackageManager pm = getActivity().getPackageManager();
+
+		Monkey monkey = new Monkey(display, packageToTest, inst, pm);
+		
+		for (int i = 0; i < NUM_EVENTS; i++){
+			monkey.nextRandomEvent();
+		}
+		
+	}
+	
+	/**
+	 * Trigger the monkey tester
+	 */
+	public void testMonkeyEvents5(){
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		Instrumentation inst = getInstrumentation();
+		PackageManager pm = getActivity().getPackageManager();
+
+		Monkey monkey = new Monkey(display, packageToTest, inst, pm);
+		
+		for (int i = 0; i < NUM_EVENTS; i++){
+			monkey.nextRandomEvent();
+		}
+		
+	}
+	
+	/**
+	 * Trigger the monkey tester
+	 */
+	public void testMonkeyEvents6(){
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		Instrumentation inst = getInstrumentation();
+		PackageManager pm = getActivity().getPackageManager();
+
+		Monkey monkey = new Monkey(display, packageToTest, inst, pm);
+		
+		for (int i = 0; i < NUM_EVENTS; i++){
+			monkey.nextRandomEvent();
+		}
+		
+	}
+	
+	/**
+	 * Trigger the monkey tester
+	 */
+	public void testMonkeyEvents7(){
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		Instrumentation inst = getInstrumentation();
+		PackageManager pm = getActivity().getPackageManager();
+
+		Monkey monkey = new Monkey(display, packageToTest, inst, pm);
+		
+		for (int i = 0; i < NUM_EVENTS; i++){
+			monkey.nextRandomEvent();
+		}
+		
+	}
+	
+	/**
+	 * Trigger the monkey tester
+	 */
+	public void testMonkeyEvents8(){
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		Instrumentation inst = getInstrumentation();
+		PackageManager pm = getActivity().getPackageManager();
+
+		Monkey monkey = new Monkey(display, packageToTest, inst, pm);
+		
+		for (int i = 0; i < NUM_EVENTS; i++){
+			monkey.nextRandomEvent();
+		}
+		
+	}
+	
+	/**
+	 * Trigger the monkey tester
+	 */
+	public void testMonkeyEvents9(){
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		Instrumentation inst = getInstrumentation();
+		PackageManager pm = getActivity().getPackageManager();
+
+		Monkey monkey = new Monkey(display, packageToTest, inst, pm);
+		
+		for (int i = 0; i < NUM_EVENTS; i++){
+			monkey.nextRandomEvent();
+		}
+		
+	}
+	
+	/**
+	 * Trigger the monkey tester
+	 */
+	public void testMonkeyEvents10(){
 		Display display = getActivity().getWindowManager().getDefaultDisplay();
 		Instrumentation inst = getInstrumentation();
 		PackageManager pm = getActivity().getPackageManager();
@@ -36,17 +182,18 @@ public class RandomTest_A extends ActivityInstrumentationTestCase2<StartPageActi
 	
 	@Override
 	protected void setUp() throws Exception {
-		// TODO Auto-generated method stub
 		super.setUp();
-		
 		setActivityInitialTouchMode(false);
+		robotiumSolo = new Solo(getInstrumentation(), getActivity());
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
-		// TODO Auto-generated method stub
+		robotiumSolo.finishOpenedActivities();
 		super.tearDown();
+		
 	}
+	
 	
 
 }
